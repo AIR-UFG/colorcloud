d6db909e0531d671633168eac592d24f0feca55a
Lucas Pereira <apereiral@ufg.br>
2024-05-10 11:37:14 -0300

    finished riu-net

--- colorcloud/models.py
+++ colorcloud/models.py
@@ -4,7 +4,8 @@
 __all__ = ['Block', 'Encoder', 'Decoder', 'UNet']
 
 # %% ../nbs/01_models.ipynb 4
-from torch.nn import Module, Sequential, Conv2d, BatchNorm2d, ReLU, ModuleList, MaxPool2d
+import torch
+from torch.nn import Module, Sequential, Conv2d, BatchNorm2d, ReLU, ModuleList, MaxPool2d, ConvTranspose2d
 
 # %% ../nbs/01_models.ipynb 5
 class Block(Module):
@@ -12,10 +13,10 @@ class Block(Module):
         super().__init__()
         self.net = Sequential(
             Conv2d(in_channels, out_channels, 3, 1, 1, bias=False, padding_mode='circular'), 
-            BatchNorm2d(out_channels, momentum = 0.99), 
+            BatchNorm2d(out_channels), 
             ReLU(),
             Conv2d(out_channels, out_channels, 3, 1, 1, bias=False, padding_mode='circular'), 
-            BatchNorm2d(out_channels, 1e-05, 0.99), 
+            BatchNorm2d(out_channels), 
             ReLU(),
         )
     
@@ -24,23 +25,22 @@ class Block(Module):
 
 # %% ../nbs/01_models.ipynb 8
 class Encoder(Module):
-    def __init__(self, channels=(2, 64, 128, 256, 512, 1024)):
+    def __init__(self, channels=(5, 64, 128, 256, 512, 1024)):
         super().__init__()
-        self.encBlocks = ModuleList(
+        self.blocks = ModuleList(
             [Block(channels[i], channels[i+1]) for i in range(len(channels)-1)]
         )
         self.pool = MaxPool2d(2)
     
     def forward(self, x):
-        "Pass the inputs into the current encoder block, store the block outputs and aply the max-pooling."
-        blockOutputs = []
-        for block in self.encBlocks:
+        enc_features = []
+        for block in self.blocks:
             x = block(x)
-            blockOutputs.append(x)
+            enc_features.append(x)
             x = self.pool(x)
-        return x, blockOutputs
+        return enc_features
 
-# %% ../nbs/01_models.ipynb 9
+# %% ../nbs/01_models.ipynb 10
 class Decoder(Module):
     def __init__(self, channels=(1024, 512, 256, 128, 64)):
         super().__init__()
@@ -48,38 +48,30 @@ class Decoder(Module):
         self.upconvs = ModuleList(
             [ConvTranspose2d(channels[i], channels[i+1], 2, 2) for i in range(len(channels)-1)]
         )
-        self.dec_blocks = ModuleList(
+        self.blocks = ModuleList(
             [Block(channels[i], channels[i+1]) for i in range(len(channels)-1)]
         )
     
-    def forward(self, x, encFeatures):
+    def forward(self, enc_features):
+        x = enc_features[-1]
         for i in range(len(self.channels)-1):
             x = self.upconvs[i](x)
-            encFeat = self.crop(encFeatures[i], x)
-            x = torch.cat([x, encFeat], dim=1)
-            x = self.dec_blocks[i](x)
+            x = torch.cat([x, enc_features[-(i+2)]], dim=1)
+            x = self.blocks[i](x)
         return x
-    
-    def crop(self, encFeatures, x):
-        (_, _, H, W) = x.shape
-        encFeatures = CenterCrop([H, W])(encFeatures)
-        return encFeatures
 
-# %% ../nbs/01_models.ipynb 10
+# %% ../nbs/01_models.ipynb 12
 class UNet(Module):
-    def __init__(self, encChannels=(2, 64, 128, 256, 512, 1024), decChannels=(1024, 512, 256, 128, 64), nbClasses=20):
+    def __init__(self, in_channels=5, hidden_channels=(64, 128, 256, 512, 1024), n_classes=20):
         super().__init__()
-        self.encoder = Encoder(encChannels)
-        self.decoder = Decoder(decChannels)
-        
-        # Regression head
-        self.head = Conv2d(decChannels[-1], nbClasses, 1)
+        self.backbone = Sequential(
+            Encoder((in_channels, *hidden_channels)),
+            Decoder(hidden_channels[::-1])
+        )
+        self.head = Conv2d(hidden_channels[0], n_classes, 1)
     
     def forward(self, x):
-        encFeatures = self.encoder(x)
-        decFeatures = self.decoder(encFeatures[::-1][0], encFeatures[::-1][1:])
+        features = self.backbone(x)
+        prediction = self.head(features)
         
-        # segmentation map
-        projection = self.head(decFeatures)
-        
-        return projection
+        return prediction
